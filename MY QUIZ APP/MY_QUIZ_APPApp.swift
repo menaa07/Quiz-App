683920603cfe185c3bdfc6d06cd3a063d5233cce
mena <stuf408478@ese.gov.ae>
2023-08-12 17:41:59 +0400

    Initial Commit

---
+++ MY QUIZ APP/MY_QUIZ_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MY_QUIZ_APPApp.swift
+//  MY QUIZ APP
+//
+//  Created by Mena Haitham on 12/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MY_QUIZ_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
